fbcd3d3825847a83dbae208afac599f3bd90a839
JL <3203679029@qq.com>
2019-10-25 14:36:49 +0800

    four commit

---
+++ src/main/java/com/lang/springboot_e/fortest/TestBean.java
@@ -0,0 +1,18 @@
+package com.lang.springboot_e.fortest;
+
+public class TestBean {
+    private String content;
+
+    public TestBean(String content){
+        super();
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
